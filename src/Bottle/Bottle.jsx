ec6a5b99086b09a2ffc72f1d55816e5d3b4bc75e
Paul Shynkaruk <overkloker33@gmail.com>
2024-03-11 14:44:50 +0200

    add Bottle comp

--- src/Bottle/Bottle.jsx
+++ src/Bottle/Bottle.jsx
@@ -1,11 +1,77 @@
-import { useMemo, useState } from 'react';
+import {Img, Sequence, spring, useCurrentFrame, useVideoConfig, interpolate} from "remotion";
+import Image from './../assets/jack.png'
 
 const Bottle = () => {
- return (
-  <div>
-   Bottle
-  </div>
- );
+    const frame = useCurrentFrame();
+    const {fps,durationInFrames} = useVideoConfig();
+    const scale = spring({
+        fps,
+        frame,
+    });
+
+    const backgroundColor = `rgb(${Math.floor((255 * frame) / durationInFrames)}, 0, ${Math.floor(255 - (255 * frame) / durationInFrames)})`;
+
+
+    return (
+        <div style={{
+            backgroundColor,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+        }}>
+            <div
+                style={{
+                    textAlign: "center",
+                    fontSize: "7em",
+                }}
+            >
+                <div style={{
+                    transform: `scale(${scale})`,
+                    display: 'flex',
+                    justifyContent: 'flex-end',
+                    color: '#08a2c5',
+                    fontWeight: 'bold'
+                }}>whiskey jack daniels
+                </div>
+            </div>
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column'
+            }}>
+                <div style={{
+                    width: 500,
+                }}>
+                    <Img src={Image} alt="fffff"/>
+                </div>
+            </div>
+            <div style={{
+                display: 'flex',
+                justifyContent: 'flex-end',
+                paddingRight: 50,
+                color: '#bb0d75',
+                fontWeight: 'bold',
+                height: 120
+            }}>
+                <Sequence from={35} layout="none">
+                <span style={{
+                    fontSize: 100,
+                }}>$150</span>
+                </Sequence>
+            </div>
+            <div
+                style={{
+                    justifyContent: "center",
+                    alignItems: "center",
+                    fontSize: 100,
+                    color: '#fff',
+                    fontWeight: 'bold',
+                }}
+            >
+                Frame: {frame}.
+            </div>
+        </div>
+
+    );
 };
 
 export default Bottle;
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,44 +1,22 @@
 import {Composition} from 'remotion';
-import {HelloWorld, myCompSchema} from './HelloWorld';
+import Bottle from './Bottle';
 import {Logo, myCompSchema2} from './HelloWorld/Logo';
 
 // Each <Composition> is an entry in the sidebar!
+const FPS = 30;
+// duration 10s
+const DURATION = FPS * 10
 
 export const RemotionRoot: React.FC = () => {
 	return (
 		<>
 			<Composition
-				// You can take the "id" to render a video:
-				// npx remotion render src/index.ts <id> out/video.mp4
-				id="HelloWorld"
-				component={HelloWorld}
-				durationInFrames={150}
-				fps={30}
-				width={1920}
-				height={1080}
-				// You can override these props for each render:
-				// https://www.remotion.dev/docs/parametrized-rendering
-				schema={myCompSchema}
-				defaultProps={{
-					titleText: 'Welcome to Remotion',
-					titleColor: '#000000',
-					logoColor1: '#91EAE4',
-					logoColor2: '#86A8E7',
-				}}
-			/>
-			{/* Mount any React component to make it show up in the sidebar and work on it individually! */}
-			<Composition
-				id="OnlyLogo"
-				component={Logo}
-				durationInFrames={150}
-				fps={30}
-				width={1920}
-				height={1080}
-				schema={myCompSchema2}
-				defaultProps={{
-					logoColor1: '#91dAE2' as const,
-					logoColor2: '#86A8E7' as const,
-				}}
+				id="MyBottleComposition"
+				durationInFrames={DURATION}
+				fps={FPS}
+				width={720}
+				height={1280}
+				component={Bottle}
 			/>
 		</>
 	);
